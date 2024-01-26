558c3a3d730a0eeb4e2e93c82c7f2fcf4123adbb
Saara Korhonen <korhonensaara@hotmail.com>
2024-01-27 00:19:06 +0200

    Grabbing objects

--- Assets/HandInput.cs
+++ Assets/HandInput.cs
@@ -8,6 +8,11 @@ public class HandInput : MonoBehaviour
 
     public float RotationSpeed = 100f;
 
+
+    public KeyCode actionKey = KeyCode.Space;
+    private bool isHolding = false;
+    private GameObject heldObject;
+
     public GameObject HandObject;
 
     private void Update()
@@ -24,7 +29,6 @@ public class HandInput : MonoBehaviour
         // float rotation2Input = Input.GetAxis("Rotation2");
 
         // Calculate movement direction
-        // Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
         Vector3 movement = new Vector3(horizontalInput, updownInput, verticalInput);
         movement = Camera.main.transform.TransformDirection(movement);
 
@@ -34,12 +38,61 @@ public class HandInput : MonoBehaviour
         if (HandObject)
         {
             HandObject.transform.Rotate(Vector3.forward * rotationInput * RotationSpeed * Time.deltaTime);
-            // HandObject.transform.Rotate(Vector3.up * rotation2Input * RotationSpeed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKeyDown(actionKey))
+        {
+            if (!isHolding)
+            {
+                // Try to pick up an object
+                TryPickUpObject();
+            }
+            else
+            {
+                // Release the held object
+                ReleaseHeldObject();
+            }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("OnTriggerEnter " + other.gameObject.name);
+        if (other.CompareTag("Grabbable"))
+        {
+            heldObject = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // Check if the collider belongs to the held object
+        if (other.gameObject == heldObject)
+        {
+            heldObject = null;
+        }
+    }
+
+    void TryPickUpObject()
+    {
+        if (heldObject == null)
+            return;
+
+        isHolding = true;
+        heldObject.GetComponent<Rigidbody>().isKinematic = true; // Disable physics on the object
+        heldObject.transform.parent = HandObject.transform; // Attach the object to the hand
+    }
+
+    void ReleaseHeldObject()
+    {
+        if (heldObject == null)
+            return;
+
+        // Release the held object
+        isHolding = false;
+
+        heldObject.transform.parent = null; // Detach the object from the hand
+        heldObject.GetComponent<Rigidbody>().isKinematic = false; // Enable physics on the object
+        heldObject = null;
     }
 }
