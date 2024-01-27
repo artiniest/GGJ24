c631bf93ccba1a2f329909a3f7b1cfb7908e5cda
Saara Korhonen <korhonensaara@hotmail.com>
2024-01-27 13:03:37 +0200

    Player Input changes

---
+++ Assets/Hand.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public GameObject RotationObject;
+
+    private GameObject _heldObject;
+    public GameObject HeldObject => _heldObject;
+
+    private bool _isHolding;
+    public bool IsHolding => _isHolding;
+
+    // Move to Hand.cs
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grabbable"))
+        {
+            _heldObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the collider belongs to the held object
+        if (other.gameObject == HeldObject)
+        {
+            _heldObject = null;
+        }
+    }
+
+    public void TryPickUpObject()
+    {
+        if (_heldObject == null)
+            return;
+
+        _isHolding = true;
+        _heldObject.GetComponent<Rigidbody>().isKinematic = true; // Disable physics on the object
+        _heldObject.transform.parent = transform; // Attach the object to the hand
+    }
+
+    public void ReleaseHeldObject()
+    {
+        if (_heldObject == null)
+            return;
+
+        // Release the held object
+        _isHolding = false;
+
+        _heldObject.transform.parent = null; // Detach the object from the hand
+        _heldObject.GetComponent<Rigidbody>().isKinematic = false; // Enable physics on the object
+        _heldObject = null;
+    }
+}
