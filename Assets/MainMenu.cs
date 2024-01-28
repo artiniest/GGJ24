292c36628c3a26218b5a9508b64543611eb13b24
Aleksi Koskinen <aleksi.koskinen91@gmail.com>
2024-01-28 12:15:54 +0200

    Create credits menu, fix items location in the menu.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,6 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenuCanvas;
+    public GameObject creditsCanvas;
+
+    public void ShowCredits()
+    {
+        mainMenuCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        creditsCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(true);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game"); // Replace with your game scene name
