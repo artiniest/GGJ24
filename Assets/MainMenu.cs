ca37f81453e9e2fc48ea8e3d16190bf22840f70f
Aleksi Koskinen <aleksi.koskinen91@gmail.com>
2024-01-28 14:45:40 +0200

    Add menubutton sounds.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,21 +5,43 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenuCanvas;
     public GameObject creditsCanvas;
+    public AudioClip selectSound; // Assign this in the Inspector
+    private AudioSource audioSource;
+
+
+    public float delayInSeconds = 1f;
+    
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void ShowCredits()
     {
+        audioSource.PlayOneShot(selectSound);
         mainMenuCanvas.SetActive(false);
         creditsCanvas.SetActive(true);
     }
 
     public void BackToMainMenu()
     {
+        audioSource.PlayOneShot(selectSound);
         creditsCanvas.SetActive(false);
         mainMenuCanvas.SetActive(true);
     }
 
     public void StartGame()
     {
+        audioSource.PlayOneShot(selectSound);
+
+        Invoke(nameof(LoadGameScene), delayInSeconds);
+        // SceneManager.LoadScene("Game"); // Replace with your game scene name
+    }
+
+    private void LoadGameScene()
+    {
+        // Load your game scene
         SceneManager.LoadScene("Game"); // Replace with your game scene name
     }
 
